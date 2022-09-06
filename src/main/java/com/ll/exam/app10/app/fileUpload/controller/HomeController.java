fd8c9e268015da1fe6edc4139f9d3d9145b289a2
jihun-24k <gi020477@gmail.com>
2022-09-06 17:27:02 +0900

    Feat: 회원가입 폼 작성

--- src/main/java/com/ll/exam/app10/app/fileUpload/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/fileUpload/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ll.exam.app10.app.home.controller;
+package com.ll.exam.app10.app.fileUpload.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;

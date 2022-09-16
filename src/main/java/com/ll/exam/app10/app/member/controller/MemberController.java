e1c3c0c7f91fde7c2f2ef4ec388eb3e16fedbbdd
jihun-24k <gi020477@gmail.com>
2022-09-16 13:37:54 +0900

    Feat: 이미지를 경유해서 보여줌

--- src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletException;
@@ -68,5 +65,10 @@ public class MemberController {
     @GetMapping("/login")
     public String showLogin() {
         return "member/login";
+    }
+
+    @GetMapping("/profile/img/{id}")
+    public String showProfileImg(@PathVariable Long id) {
+        return "redirect:" + memberService.getMemberById(id).getProfileImgUrl();
     }
 }
